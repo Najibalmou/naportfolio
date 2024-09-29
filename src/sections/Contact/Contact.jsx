66edf4bccb107f07f2a5799848b52333e287915c
Najibalmou <144485675+Najibalmou@users.noreply.github.com>
2024-09-29 13:51:34 +0200

    Update Contact.jsx

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form ref={form} onSubmit={sendEmail}>
+      <form>
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
