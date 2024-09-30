df6efca79f6ce45c9da2bbda3789e6c7d1abea25
Najibalmou <144485675+Najibalmou@users.noreply.github.com>
2024-09-30 19:33:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // If EmailJs needs to be treated as an external dependency, add it here
+      external: ['emailjs'],
+    },
+  },
 })
